9f013c941f33b69b4e0187dfb635059bd7758d1c
GrayWorldFinex <116639741+GrayWorldFinex@users.noreply.github.com>
2022-11-07 09:19:05 +0200

    Add files via upload

---
+++ GetDiskInformation/GetDiskInf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDiskInformation
+{
+    public class GetDiskInform
+    {
+        public string GetDiskInf()
+        {
+            string inf = String.Empty;
+            DriveInfo[] info = DriveInfo.GetDrives();
+            foreach(DriveInfo x_info in info)
+            {
+                inf = x_info.Name + Environment.NewLine + x_info.VolumeLabel + Environment.NewLine + x_info.DriveFormat;
+            }
+            return inf; //Getting Disk Information :D
+        }
+    }
+}
